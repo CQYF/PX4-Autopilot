zevaaero/PX4-ZEVA

PreFlightCheck: Detect missing FMU SD card
Signed-off-by: Claudio Micheli <claudio@auterion.com>

src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.cpp
@@ -80,6 +80,7 @@ bool PreFlightCheck::preflightCheck(orb_advert_t *mavlink_log_pub, vehicle_statu
 
 	failed = failed || !externalUpdateCheck(mavlink_log_pub, reportFailures);
 	failed = failed || !airframeCheck(mavlink_log_pub, status);
+	failed = failed || !sdcardCheck(mavlink_log_pub, reportFailures);
 
 	/* ---- MAG ---- */
 	if (checkSensors) {

CQYF/PX4-Autopilot

src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.cpp
@@ -64,6 +64,7 @@ bool PreFlightCheck::preflightCheck(orb_advert_t *mavlink_log_pub, vehicle_statu
 	bool failed = false;
 
 	failed = failed || !airframeCheck(mavlink_log_pub, status);
+	failed = failed || !sdcardCheck(mavlink_log_pub, report_failures);
 
 	/* ---- MAG ---- */
 	{