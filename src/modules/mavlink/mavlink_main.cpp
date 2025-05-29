69bd8c1542b00a918707f0d361a0da196bd70e9a
Cao Yue <caoyue_nudt@163.com>
2025-05-29 09:43:36 +0800

    mavdebug修改，可以实时显示数据曲线

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1615,6 +1615,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 
 	/* fallthrough */
 	case MAVLINK_MODE_CUSTOM:
+		configure_stream_local("DEBUG_FLOAT_ARRAY", 50.0f);
 		//stream nothing
 		break;
 
